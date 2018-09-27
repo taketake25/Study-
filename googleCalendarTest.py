a3b614bae0ff3cfb8b261bca26f16b2145759398
taketake25 <takeru31415@gmail.com>
2018-09-28 00:12:52 +0900

    get sample program

---
+++ googleCalendarTest.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import datetime
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES ='https://www.googleapis.com/auth/calendar.readonly'
+CLIENT_SCOPE_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Study# google API test'
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'calendar-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SCOPE_FILE, SCOPES)
+        flow.user_agent=APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow,store,flags)
+        else:
+            credentials = tools.run(flow,store)
+        print('Storing credentials to '+credential_path)
+    return credentials
+
+def main():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service=discovery.build('calendar','v3',http=http)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    print('Getting the upcoming 10 events')
+    eventsResult = service.events().list(
+        calendarId='primary',timeMin=now,maxResults=10,singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items',[])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('datatime',event['start'].get('data'))
+        print(start,event['summary'])
+        print(event['start'])
+
+if __name__ =='__main__':
+
+main()
